6035b505cbd1af946aa96646c0d4f72439ee091b
jobayer58 <jobayerafnan770@gmail.com>
2025-02-10 00:43:28 +0600

    update cute

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,11 +1,18 @@
 import { useLoaderData } from "react-router-dom";
+import NewsCard from "../components/NewsCard";
 
 const CategoryNews = () => {
-    const {data} = useLoaderData()
-    console.log(data)
+    const {data: news} = useLoaderData()
+    console.log(news)
     return (
         <div>
-            <h1>{data.length} News Found in This Category</h1>
+            <h1 className="font-semibold mb-3">Dragon News Home</h1>
+            <h2 className="text-gray-400 text-sm">{news.length} News Found in This Category</h2>
+            <div>
+                {
+                    news.map(singleNews => <NewsCard key={singleNews._id} news={singleNews}></NewsCard>)
+                }
+            </div>
         </div>
     );
 };
