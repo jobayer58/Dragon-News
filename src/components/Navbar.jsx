fa6b13d8bd682633d0f3f26b9946a2646ba58b71
jobayer58 <jobayerafnan770@gmail.com>
2025-02-14 07:03:43 +0600

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
                 <Link to='/about'>About</Link>
                 <Link to='/career'>Career</Link>
                 <Link to='/dev'>Dev information</Link>
-                <Link to='/hefajot'>hefajot</Link>
             </div>
             <div className="login flex gap-2 items-center">
                 <div className="">
